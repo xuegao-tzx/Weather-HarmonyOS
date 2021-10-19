b4db906ee7d8af61e9760f061ed57481af50e25f
xuegao <you@example.com>
2021-10-19 21:50:44 +0800

    2.0

--- MyApplication/entry/src/main/java/com/heweather/owp/dataInterface/DataInterface.java
+++
@@ -1,7 +0,0 @@
-package com.heweather.owp.dataInterface;
-/**/
-public interface DataInterface {
-    void setCid(String cid);
-    void deleteID(int index);
-    void changeBack(String condCode);
-}
--- MyApplication/entry/src/main/java/com/heweather/owp/view/horizonview/ScrollWatcher.java
+++
@@ -1,9 +0,0 @@
-package com.heweather.owp.view.horizonview;
-
-/**
- * Created by dongua on 17-9-11.
- */
-
-public interface ScrollWatcher {
-    void update(int scrollX);
-}
